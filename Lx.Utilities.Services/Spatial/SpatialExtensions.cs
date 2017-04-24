92b25e67a9a0411e3be80f045441e33ddbe48d78
lixin <lixin.yang@vontechnology.com>
2017-04-24 12:46:47 +1000

    a

---
+++ Lx.Utilities.Services/Spatial/SpatialExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity.Spatial;
+
+namespace Lx.Utilities.Services.Spatial {
+    public static class SpatialExtensions {
+        public static DbGeography FromLongitudeAndLatitude(decimal longitude, decimal latitude) {
+            var location = DbGeography.FromText($"POINT({longitude} {latitude})");
+            return location;
+        }
+    }
+}

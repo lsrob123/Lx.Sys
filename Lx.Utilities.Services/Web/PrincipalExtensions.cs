4923132ebb5d0e4560a88f5a87a8a339194d93f5
lixin <lixin.yang@vontechnology.com>
2017-05-01 11:32:06 +1000

    a

---
+++ Lx.Utilities.Services/Web/PrincipalExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Principal;
+using Lx.Utilities.Contract.Authentication.DTOs;
+using Lx.Utilities.Services.Authentication;
+
+namespace Lx.Utilities.Services.Web {
+    public static class PrincipalExtensions {
+        public static IdentityDto Current<TPrincipal>(this TPrincipal principal) where TPrincipal : IPrincipal {
+            return new IdentityDto().WithPrincipal(principal);
+        }
+    }
+}

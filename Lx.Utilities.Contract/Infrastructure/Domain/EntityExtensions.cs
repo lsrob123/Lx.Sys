681e12ea6bec00ac4f3b170fad64481e8636713e
Lixin Yang <lsrob@hotmail.com>
2017-05-10 00:00:50 +1000

    b

--- Lx.Utilities.Contract/Infrastructure/Domain/EntityExtensions.cs
+++ Lx.Utilities.Contract/Infrastructure/Domain/EntityExtensions.cs
@@ -1,34 +1,41 @@
 ﻿using System;
 
-namespace Lx.Utilities.Contract.Infrastructure.Domain {
-    public static class EntityExtensions {
+namespace Lx.Utilities.Contract.Infrastructure.Domain
+{
+    public static class EntityExtensions
+    {
         public static TEntity WithKey<TEntity>(this TEntity entity, Guid key)
-            where TEntity : IEntity {
+            where TEntity : IEntity
+        {
             entity.SetKey(key);
             return entity;
         }
 
         public static TEntity WithId<TEntity>(this TEntity entity, long id)
-            where TEntity : IEntity {
+            where TEntity : IEntity
+        {
             entity.SetId(id);
             return entity;
         }
 
         public static TEntity WithTimeCreated<TEntity>(this TEntity entity, DateTimeOffset? timeCreated)
-            where TEntity : IEntity {
+            where TEntity : IEntity
+        {
             entity.SetTimeCreated(timeCreated);
             return entity;
         }
 
         public static TEntity WithTimeModified<TEntity>(this TEntity entity, DateTimeOffset? timeModified)
-            where TEntity : IEntity {
+            where TEntity : IEntity
+        {
             entity.SetTimeModified(timeModified);
             return entity;
         }
 
         public static TEntity AsNewEntity<TEntity>(this TEntity entity, bool enforceCurrentTimeToTimeCreated = true,
             bool enforceCurrentTimeToTimeModified = true)
-            where TEntity : IEntity {
+            where TEntity : IEntity
+        {
             entity.EnsureValidKey();
             if (enforceCurrentTimeToTimeCreated)
                 entity.WithTimeCreated(DateTimeOffset.UtcNow);

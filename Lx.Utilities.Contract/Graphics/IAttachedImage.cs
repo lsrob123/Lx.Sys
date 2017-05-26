b7ad0169082f4c8c4214c268909f799ab9b9477d
lixin <lixin.yang@vontechnology.com>
2017-05-26 14:28:56 +1000

    a

---
+++ Lx.Utilities.Contract/Graphics/IAttachedImage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lx.Utilities.Contract.Graphics {
+    public interface IAttachedImage<out TImage> where TImage : IImageSafe {
+        Guid OwnerObjectKey { get; }
+        int DisplayOrder { get; }
+        TImage Image { get; }
+        Guid Key { get; }
+    }
+}
